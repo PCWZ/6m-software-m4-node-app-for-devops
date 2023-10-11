38b597a861ec673da689f3ad10fa96d8b39ccc83
PCWZ <parrychuawz@gmail.com>
2023-10-11 21:48:45 +0800

    edited config

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 
 // This is the only function that will be called by the endpoint.
 function print(req, res){
-    res.send("Hello world!");
+    res.send("Hello I'm Parry! successfuly depluy my app!");
 }
 
 module.exports = print;
--- controller.test.js
+++ controller.test.js
@@ -7,13 +7,13 @@ describe("the print function", ()=>{
         jest.clearAllMocks();
     })
 
-    it("should respond with 'Hello world!'", ()=>{
+    it("should respond with 'Hello I'm Parry! successfuly depluy my app!'", ()=>{
         const mockReq = {};
         const mockRes = {
             send:jest.fn()
         }
 
         targetFn(mockReq, mockRes);
-        expect(mockRes.send).toHaveBeenCalledWith("Hello world!"); // If you change this value, the test will fail.
+        expect(mockRes.send).toHaveBeenCalledWith("Hello I'm Parry! successfuly depluy my app!"); // If you change this value, the test will fail.
     })
 })
